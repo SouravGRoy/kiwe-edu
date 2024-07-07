0f1d150e9a5dbafe0430dd6f94663b491a56ff90
sourav guha roy <souravroy94375@gmail.com>
2024-07-08 01:40:48 +0530

    number ticking animation

---
+++ src/app/components/about/numberTicker.tsx
@@ -0,0 +1,42 @@
+import NumberTicker from "../magicui/number-ticker";
+
+const NumberTickerDemo = () => {
+  return (
+    <div className="flex justify-around md:flex-row flex-col font-montserrat md:p-20 p-10 md:mt-8 ">
+      <div className="text-center mb-4">
+        <p className="whitespace-pre-wrap md:text-5xl  text-4xl font-bold tracking-tighter text-blue-600">
+          <NumberTicker value={350} />
+        </p>
+        <p className="md:text-base text-sm mt-2 font-medium tracking-tighter text-black ">
+          FINISHED WEBINARS
+        </p>
+      </div>
+      <div className="text-center mb-4">
+        <p className="whitespace-pre-wrap md:text-5xl text-4xl font-bold tracking-tighter text-blue-600">
+          <NumberTicker value={100} /> %
+        </p>
+        <p className="md:text-base text-sm mt-2 font-medium tracking-tighter text-black ">
+          SATISFACTION RATE
+        </p>
+      </div>
+      <div className="text-center mb-4">
+        <p className="whitespace-pre-wrap md:text-5xl text-4xl font-bold tracking-tighter text-blue-600">
+          <NumberTicker value={656} />
+        </p>
+        <p className="md:text-base text-sm mt-2 font-medium tracking-tighter text-black ">
+          ONLINE SESSIONS
+        </p>
+      </div>
+      <div className="text-center">
+        <p className="whitespace-pre-wrap md:text-5xl text-4xl font-bold tracking-tighter text-blue-600">
+          <NumberTicker value={70} />+
+        </p>
+        <p className="md:text-base text-sm mt-2 font-medium tracking-tighter text-black ">
+          UNIQUE COURSES
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NumberTickerDemo;
