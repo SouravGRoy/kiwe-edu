1fe361a89e6343a6b9cb681eb05a64f47e170ecd
sourav guha roy <souravroy94375@gmail.com>
2024-07-07 23:42:58 +0530

    Abouts Component

---
+++ src/app/aboutUs/page.tsx
@@ -0,0 +1,19 @@
+import Companies from "../components/about/companies";
+import Content from "../components/about/content";
+import LandingAbout from "../components/about/landingAbout";
+import Footer from "../components/home/footer";
+import Navbar from "../components/home/navbar";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <LandingAbout />
+      <Content />
+      <Companies />
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
