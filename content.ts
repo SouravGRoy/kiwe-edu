1d0c64ef167015e69cfaedaad717e48324c03c19
sourav guha roy <souravroy94375@gmail.com>
2024-09-30 23:38:05 +0530

    add

--- content.ts
+++ content.ts
@@ -12,7 +12,6 @@ export type contentType = {
 
 export const content: contentType[] = [
     {
-<<<<<<< HEAD
         id: 1,
         title: "REGULAR",
         course: "SCIENCE, ARTS AND COMMERCE",
@@ -48,53 +47,8 @@ export const content: contentType[] = [
             "Focus on research and practical knowledge, with a curriculum designed to foster scientific curiosity and analytical skills in students.",
         brochure: "#",
     },
-    {
-        id: 4,
-        title: "B.A",
-        course: "Bachelor of Arts",
-        image: "/images/course.webp",
-        eligibility: "12th pass",
-        duration: "3 years",
-        courseFee: "70,000",
-        description:
-            "A comprehensive education in humanities and social sciences, encouraging critical thinking, creativity, and communication skills.",
-        brochure: "#",
-=======
-      id: 1,
-      title: "REGULAR",
-      course: "SCIENCE, ARTS AND COMMERCE",
-      image: "/images/s1.jpg",
-      eligibility: "IX AND XI",
-      duration: "APRIL (PRESENT YEAR) TO FEBRUARY (NEXT YEAR)",
-      courseFee: "1,40,000",
-      description:
-        "This comprehensive course offers tailored education for students pursuing Science, Arts, or Commerce streams, providing in-depth academic preparation from April to February. Suitable for students entering 9th and 11th grades, the course helps them build strong foundations and excel in their chosen streams.",
-      brochure:"",
-    },
-    {
-      id: 2,
-      title: "REGULAR",
-      course: "SCIENCE, ARTS AND COMMERCE",
-      image:"/images/s2.jpg",
-      eligibility: "X AND XII",
-      duration: "APRIL TO NOVEMBER OF THE SAME YEAR",
-      courseFee: "2,00,000",
-      description:
-        "This intensive course is designed for students in their final academic year of Grades 10 and 12, providing focused preparation in Science, Arts, or Commerce streams. Running from April to November, it equips students with the knowledge and skills necessary to excel in board examinations and future academic pursuits.",
-        brochure:"#",
-    },
-    {
-      id: 3,
-      title: "CRASH COURSE",
-      course: "SCIENCE, ARTS AND COMMERCE",
-      image:  "/images/s3.jpg",
-      eligibility: "X AND XII",
-      duration: "DECEMBER (1ST WEEK) TO FEBRUARY OF NEXT YEAR (1ST WEEK)",
-      courseFee: "90,000",
-      description:
-        "This crash course is designed for students in Grades 10 and 12, offering an accelerated and focused revision of the Science, Arts, or Commerce syllabus. Spanning from December to early February, this intensive program is ideal for students seeking a final boost before their board exams, ensuring they are well-prepared and confident for success.",
-        brochure:"#",
-    },
+    
+  
     {
       id: 4,
       title: "JEE EXAM",
@@ -106,7 +60,6 @@ export const content: contentType[] = [
       description:
         "This comprehensive 3-year program is tailored for students who have completed their 12th grade and are aiming to crack both JEE Main and Advanced exams. The course offers in-depth preparation, covering all essential topics, concepts, and strategies to help students achieve top ranks in India's premier engineering entrance exams.",
         brochure:"/images/Arihant Differential Calculus_unlocked.pdf",
->>>>>>> 537d043 (latest update)
     },
     {
         id: 5,
