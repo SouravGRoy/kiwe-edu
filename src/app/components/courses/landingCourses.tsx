271faa97b9f18f476fa00049dfa1c830a7622061
SouravGRoy <souravroy94375@gmail.com>
2024-10-17 18:39:45 +0530

    add

--- src/app/components/courses/landingCourses.tsx
+++ src/app/components/courses/landingCourses.tsx
@@ -2,46 +2,46 @@ import React from "react";
 import { IoIosArrowForward } from "react-icons/io";
 
 export default function LandingCourses() {
-  return (
-    <div>
-      <div
-        className="h-full w-full "
-        style={{ backgroundImage: "url('/images/background1.jpg')" }}
-      >
-        <div className="flex  justify-center items-center md:py-28 py-14 flex-col">
-          <h1 className="text-5xl text-secondary font-extrabold shadow-md rounded-full p-2  my-6">
-            Courses
-          </h1>
-          <nav className="flex" aria-label="Breadcrumb">
-            <div className="inline-flex items-center space-x-1 md:space-x-3">
-              <div className="inline-flex items-center">
-                <a
-                  href="/"
-                  className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-                >
-                  Home
-                </a>
-                <IoIosArrowForward />
-                <a
-                  href="/"
-                  className="inline-flex items-center text-sm font-medium text-blue-500 hover:text-blue-600 "
-                >
-                  Courses
-                </a>
-              </div>
+    return (
+        <div>
+            <div
+                className="h-full w-full"
+                style={{ backgroundImage: "url('/images/background1.jpg')" }}
+            >
+                <div className="flex flex-col items-center justify-center py-14 md:py-28">
+                    <h1 className="my-6 rounded-full p-2 text-5xl font-extrabold text-secondary shadow-md">
+                        Courses
+                    </h1>
+                    <nav className="flex" aria-label="Breadcrumb">
+                        <div className="inline-flex items-center space-x-1 md:space-x-3">
+                            <div className="inline-flex items-center">
+                                <a
+                                    href="/"
+                                    className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                                >
+                                    Home
+                                </a>
+                                <IoIosArrowForward />
+                                <a
+                                    href="/"
+                                    className="inline-flex items-center text-sm font-medium text-blue-500 hover:text-blue-600"
+                                >
+                                    Courses
+                                </a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
-          </nav>
+            <div className="mb-4 mt-8 flex items-center justify-center space-x-4 font-montserrat">
+                <button className="rounded-full border border-gray-200 bg-white p-2 px-4 text-xs font-semibold text-green-600 shadow-lg">
+                    <span className="mr-1 inline-flex h-2 w-2 items-center justify-center rounded-full bg-green-500"></span>
+                    EXPLORE OUR COURSES
+                </button>
+            </div>
+            <h2 className="mb-8 text-center text-4xl font-semibold">
+                Be the Best With Our Courses
+            </h2>
         </div>
-      </div>
-      <div className="flex items-center font-montserrat justify-center space-x-4 mt-8 mb-4">
-        <button className="bg-white border border-gray-200 text-green-600 rounded-full px-4 shadow-lg p-2 text-xs font-semibold">
-          <span className="inline-flex items-center justify-center  h-2 w-2 bg-green-500 rounded-full mr-1"></span>
-          EXPLORE OUR COURSES
-        </button>
-      </div>
-      <h2 className="text-4xl font-semibold text-center mb-8">
-        Master Your Brain: Personal Development
-      </h2>
-    </div>
-  );
+    );
 }
